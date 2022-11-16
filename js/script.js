0b83a0c3ed0232ec5340b42e7023c9d082e8ac07
Marek Ostrowski <rutlawski2@wp.pl>
2022-11-16 13:00:25 +0100

    Remove task option added

--- js/script.js
+++ js/script.js
@@ -6,10 +6,24 @@
         for (const task of tasks) {
             htmlString +=
                 `<li ${task.done ? "style = \"text-decoration: line-through\"" : ""}>
+                <button class="js-removeTask">Usuń!</button>
                 ${task.content};
                 </li>`;
         };
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-removeTask");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask (index);
+            });
+        });
+    }
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
     }
 
     const addNewTask = (addTaskElement) => {
@@ -18,13 +32,13 @@
     }
 
     const onFormSubmit = (event) => {
-            event.preventDefault();
-            const addTaskElement = document.querySelector(".js-addTask").value.trim();
-            if (addTaskElement === "") {
-                return;
-            }
-            addNewTask(addTaskElement);
+        event.preventDefault();
+        const addTaskElement = document.querySelector(".js-addTask").value.trim();
+        if (addTaskElement === "") {
+            return;
         }
+        addNewTask(addTaskElement);
+    }
 
     const init = () => {
         const formElement = document.querySelector(".js-form");
